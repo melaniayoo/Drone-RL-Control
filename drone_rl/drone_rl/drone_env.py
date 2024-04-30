53347dfbd3fe590b85079d06e6c0085b9b96b858
k-kaps <karan.kapoor@uwaterloo.ca>
2024-04-30 12:13:57 -0400

    added drone training parameters

--- drone_rl/drone_rl/drone_env.py
+++ drone_rl/drone_rl/drone_env.py
@@ -1,7 +1,7 @@
 import rclpy
 from gymnasium import Env
 import numpy as np
-import gymnasium.spaces import Dict, Box
+from gymnasium.spaces import Dict, Box
 import math
 from drone_control import DroneController
 
@@ -12,4 +12,32 @@ class DroneEnv(DroneController, Env):
 
         self.get_logger().info("Starting all drone controllers...")
 
+        # TODO : Establish Training Parameters | DONE
+        self.robot_name = "SJTU-Drone"
+        self._target_location = np.array([1, 10], dtype=np.float32)
+        self._initial_agent_location = np.array([1, 16, -90], dtype=np.float32)
+        
+        self._randomize_env_level = 7
+        self._normalize_obs = True
+        self._normalize_act = True
+        self._visualize_target = True
+        self._reward_method = 1
+        self._max_linear_velocity = 1
+        self._min_linear_velocity = 0
+        self._angular_velocity = 1
+        self._minimum_dist_from_target = 0.42
+        self._minimum_dist_from_obstacles = 0.26
+        
+        self._attraction_threshold = 3
+        self._attraction_factor = 1
+        self._repulsion_threshold = 1
+        self._repulsion_factor = 0.1
+        self._distance_penalty_factor = 1
+
+        self._num_steps = 0
+        self._num_episodes = 0
+        
+
+
+        # TODO : Establish the waypoints in the drone path
         
